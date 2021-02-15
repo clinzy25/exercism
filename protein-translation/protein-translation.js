b0a196fcaa45f3c8ff40e6eba5e709b50dae45df
clinzy25 <conner.linzy25@gmail.com>
2021-02-15 13:53:51 -0500

    refactor resistor and protein, remove extra gitignore

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -1,36 +1,31 @@
 export const translate = (rna = '') => {
-    if (rna === '') return [];
-    try {
-        const mapping = [
-            ['Methionine', ['AUG']],
-            ['Phenylalanine', ['UUU', 'UUC']],
-            ['Leucine', ['UUA', 'UUG']],
-            ['Serine', ['UCU', 'UCC', 'UCA', 'UCG']],
-            ['Tyrosine', ['UAU', 'UAC']],
-            ['Cysteine', ['UGU', 'UGC']],
-            ['Tryptophan', ['UGG']],
-        ];
+    const mapping = [
+        ['Methionine', ['AUG']],
+        ['Phenylalanine', ['UUU', 'UUC']],
+        ['Leucine', ['UUA', 'UUG']],
+        ['Serine', ['UCU', 'UCC', 'UCA', 'UCG']],
+        ['Tyrosine', ['UAU', 'UAC']],
+        ['Cysteine', ['UGU', 'UGC']],
+        ['Tryptophan', ['UGG']],
+    ];
 
-        let codons = [];
-        for(let i = 0; i < rna.length; i += 3) {
-            codons.push(rna.substr(i, 3));
-        }
+    //Split RNA string into 3 char codons, to match mapping[i, 1]
+    let codons = [];
+    for(let i = 0; i < rna.length; i += 3) {
+        codons.push(rna.substr(i, 3));
+    }
 
-        let protein = [];
-        loop1:
-        for(let c = 0; c < codons.length; c++) {
-            for(let i = 0; i < mapping.length; i++) {
-                if (mapping[i][1].includes(codons[c])) {
-                    protein.push(mapping[i][0]);
-                } else if (codons[c] === 'UAA' || codons[c] === 'UAG' || codons[c] === 'UGA') {
-                    break loop1;
-                } else if (!(mapping.flat(2).includes(codons[c]))) {
-                    throw error;
-                }
+    let protein = [];
+    for(let c of codons) {
+        for(let m of mapping) {
+            if (m[1].includes(c)) { //checks only first codon in mapping[i, 1]
+                protein.push(m[i][0]);
+            } else if (c === 'UAA' || c === 'UAG' || c === 'UGA') { //checks for stop codons and breaks both loop levels
+                break; 
+            } else if (!(m.flat(2).includes(c))) { 
+                throw new error('Invalid codon');
             }
         }
-        return protein;
-    } catch (e) {
-        console.error('Invalid codon');
     }
+    return protein;
 }

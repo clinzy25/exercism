c58f219df7dba92062099b055a45edb7ff7a6147
clinzy25 <conner.linzy@area4labs.com>
2023-03-13 12:43:32 -0700

    triangle v2

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -7,6 +7,5 @@ def equilateral(sides):
 def isosceles(sides):
     return len(set(sides)) == 2 and validate(sides) or equilateral(sides)
 
-
 def scalene(sides):
     return len(set(sides)) == 3 and validate(sides)

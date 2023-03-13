c1d4d661134ff0459b6304b3b1691394a2ba386f
clinzy25 <conner.linzy@area4labs.com>
2023-03-13 12:43:08 -0700

    triangle v1

---
+++ python/triangle/triangle.py
@@ -0,0 +1,12 @@
+def validate(sides):
+    return sum(sides) - max(sides) > max(sides)
+
+def equilateral(sides):
+    return len(set(sides)) == 1 and validate(sides)
+
+def isosceles(sides):
+    return len(set(sides)) == 2 and validate(sides) or equilateral(sides)
+
+
+def scalene(sides):
+    return len(set(sides)) == 3 and validate(sides)

98ece9157a2cd817c09a8f41e358471a066a19e8
clinzy25 <conner.linzy@area4labs.com>
2023-03-22 10:20:12 -0700

    difference of squares  v1

---
+++ python/difference-of-squares/difference_of_squares.py
@@ -0,0 +1,12 @@
+import functools
+
+def square_of_sum(number):
+    return sum(list(range(number + 1))) ** 2
+
+
+def sum_of_squares(number):
+    return sum(i ** 2 for i in range(number + 1))
+
+
+def difference_of_squares(number):
+    return square_of_sum(number) - sum_of_squares(number)
